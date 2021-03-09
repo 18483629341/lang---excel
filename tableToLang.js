065ad612855aaf4832ea1f8de5a02438910136be
18483629341 <714402934@qq.com>
2021-03-09 16:08:35 +0800

    Add files via upload
    下载语言包

---
+++ tableToLang.js
@@ -0,0 +1,96 @@
+/**
+ * 
+ *  @param {Array<object>} data table所对应的对象数组, [{key:'a.b.c', zhLabel: '' ,enLabel:''},...]
+ * @param {number} index 需要转出的语言包所在的列的下表，第一列下标为0，第二列下标为1
+ * @returns 
+ */
+ function getTree(data, index){
+    let tree={}
+    
+    let firstLevelPropArr=[];
+    // 生成某种语言包递归专用的数据node，含父节点的key,以及数据深度level
+    let node = getNode(data, index)
+    // node约1000多个，遍历递归数量很大1000*1000，执行次数很大，将数据分类，分别处理app/container
+    // 这样就是50*50*20 
+    let classifyNode ={};
+    // 找到所有一级的key
+    node.forEach((item)=>{
+      let oneLevelProp=item['key'].split('.')[0];
+      if(firstLevelPropArr.indexOf(oneLevelProp) === -1){
+        firstLevelPropArr.push(oneLevelProp);
+        let node=[];
+        node[0]= item;
+        classifyNode[oneLevelProp]=node;
+      }else {
+        classifyNode[oneLevelProp].push(item);
+      }
+    })
+  //   let nodeA = [
+  //     {key: "app.running", value: "dsfasdaf", parentKey: "app", level: 2},
+  //     {key: "app.status.label", value: "sfasedtqer", parentKey: "app.status", level: 3},
+  //     {key: "app.go.about", value: "weteqrt", parentKey: "app.go", level: 3},
+  //  ]
+  // tree['app'] = tranTreeObj(nodeA, 'app' , 1)
+    // 根据一级key，生成对应的对象树
+    firstLevelPropArr.forEach((prop)=>{
+      let chileTree= tranTreeObj(classifyNode[prop], prop , 1)
+       tree[prop] =chileTree
+     })
+   return tree
+ }
+
+ /**
+  * 根据一级key,生成key对应的tree对象
+  * @param {Array<object>} node table所对应的对象数组,第一列数据必须为key, 'app'/'app.status'/'app.go'
+  * 
+  * 
+  */
+  function tranTreeObj(node, parentKey, level) {
+   
+   let result={}
+   node.forEach((item)=>{
+    // console.log(item)
+    // console.log(parentKey)
+     let _levelKey = item.key.split('.')[level]
+     // 找到所有含有parentKey为的obj, 条件1：_levelKey存在
+     if(item.key.indexOf(parentKey)> -1 && _levelKey){
+        // 层级小于对象原本的层级
+        if(level < item.level-1){
+            let _pointIndex=item.parentKey.indexOf(_levelKey)+_levelKey.length
+            let _currentKey= item['key'].substring(0,_pointIndex)
+            
+            result[_levelKey] = tranTreeObj(node, _currentKey, level+1)
+         
+        // 如果没有子属性，直接将value赋值
+        } else {
+          result[_levelKey]= item.value
+        }
+     } 
+   })
+   return result
+ }
+
+ /**
+  * 生成递归专用的数据，含父节点的key,以及数据深度level
+  */
+ function getNode(data, langLabel){
+    let node=[]
+    data.forEach((item)=>{
+        // 找到父属性key
+        
+        let itemKey= item['key']
+        let lastPointIndex=itemKey.lastIndexOf('.')
+        let parentKey= itemKey.substring(0,lastPointIndex)
+        let obj = {
+          key: itemKey,
+          value: item[langLabel],
+          parentKey,
+          level: item['key'].split('.').length
+        }
+        node.push(obj)  
+    })
+    return node
+ }
+ export default {
+    getTree
+ }
---
+++ tabley.vue
@@ -0,0 +1,157 @@
+<template>
+	<section class="table-container">
+      <b>table</b>
+      <div>
+        <div class="ant-modal-footer" style="text-align: left;">
+          <button class="primary" @click="exportExcel">导出excel</button>
+          
+        </div>
+        <div class="ant-modal-footer" style="text-align: right;">
+          
+          <button @click="importE">导入excel</button>
+          <button class="primary" @click="replaceLang('en')" >替换英文</button>
+        
+          <button class="primary" @click="generateEnLang('enLabel')">生成英文</button>
+        </div>
+        
+      </div>
+      
+      <table class="table" id="myTab">
+        <thead>
+           <tr>
+            <td class="td"><b>key</b></td>
+            <td class="td"><b>中文</b></td>
+            <td class="td"><b>英文</b></td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="(row, index) in tableScoreData"
+            :key="index"
+          >
+            <td v-for="(item, i) in row" :key="i" class="td">{{item}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
+</template>
+
+<script>
+import tableToExcel from '../mixins/tableToExcel'
+import init from '../mixins/langToTable'
+import Excel from '../mixins/excel'
+import FileSaver from 'file-saver'
+import getTree  from '../mixins/tableToLang'
+
+export default {
+	data() {
+        return {
+        tableScoreData: [   ],
+        newData:{},
+        canReplace: false,
+        zhMap: null,
+        enMap: null
+      }
+    },
+	mixins: [tableToExcel, init],
+  created(){
+    this.tableScoreData = this.init()
+    this.importE()
+  },
+	methods: { 
+      exportExcel() {
+            this.tableToExcel([
+              {
+                  id: 'myTab',
+                  name: 'zeus中英文统计'
+              }
+            ], '国际化统计')
+          
+      },
+      importE(){
+        // 保存数据，生成中英文map数据
+        Excel.importExcel((data, dataRef) => {
+          this.zhMap = new Map()
+          this.enMap = new Map()
+           console.log(data);
+          data.forEach((item,i)=>{
+            if(i>0){
+              let key=item[0];
+              let zhValue = item[1] || '';
+              let enValue = item[2] || '';
+              this.zhMap.set(key, zhValue);
+              this.enMap.set(key, enValue);
+            }
+          })
+          this.canReplace =true
+        })
+      },
+      /**
+       *  
+       */
+      replaceLang(lang){
+        let arr = [...this.tableScoreData];
+        let map = new Map()
+        switch(lang){
+          case 'en':
+            arr.forEach((item)=>{
+              if(item[lang+'label']!== this.enMap.get(item['key'])){
+                item[lang+'label'] = this.enMap.get(item['key']);
+              } 
+            });
+            break;
+          case 'zh':
+            arr.forEach((item)=>{
+              if(item[lang+'label']!== this.zhMap.get(item['key'])){
+                item[lang+'label'] = this.zhMap.get(item['key']);
+              } 
+            });
+            break;
+          default:
+            console.err('需要添加语言map')
+            break;
+            
+        }
+       
+        // console.log(arr);
+        this.tableScoreData = [...arr]
+      },
+      generateEnLang(label){
+        let tableData=JSON.parse(JSON.stringify(this.tableScoreData))
+        // 生成lang树
+        // 参数1为表格数据 ，参数2为需要转出语言包的所在列的下标；比如英语所在列为2
+        let enTree=getTree.getTree(tableData, 'enLabel');
+        // console.log(enTree)
+        // 将json转换成字符串
+        let data = JSON.stringify(enTree)
+              data = 'export default' + data
+        const blob = new Blob([data], {type: ''})
+        FileSaver.saveAs(blob, 'en.ts')
+      }
+
+    }
+}
+</script>
+
+<style>
+.table-container {
+  background-color: #fff;
+  margin: 20px 0;
+}
+.table {
+  width: 100%;
+  margin-bottom: 30px;
+  border-collapse: collapse;
+}
+tr:first-child {
+  .td {
+    font-weight: bold;
+  }
+}
+.td {
+  line-height: 40px;
+  border: 1px solid #e8e8e8;
+  text-align: center;
+  font-size: 14px;
+}
+</style>
